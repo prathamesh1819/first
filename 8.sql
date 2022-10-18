2bf9c0e71c6503960ef27d0139b4a560495ab5c1
prathamesh1819 <prathameshshinde1718@gmail.com>
2022-10-18 16:07:31 +0530

    sql file added by prathamesh

--- 8.sql
+++ 8.sql
@@ -4,7 +4,7 @@ create database product;
 use product;
 
 create table info(pid int primary key auto_increment,pname varchar(20),uprice float,ustock int, uorder int); 
-
+drop table info ;
 insert into info(pname,uprice,ustock,uorder)
 values("MOUSE",55,15,16),("KEYBOARD",90,12,19),("SPEAKER",100,16,14),("HEADPHONE",75.6,14,null);
 
