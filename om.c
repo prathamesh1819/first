72ae7a70d8f3211cdb5e1cd4066d5488fcb06651
prathamesh1819 <prathameshshinde1718@gmail.com>
2022-10-19 12:11:03 +0530

    chnages made in om.c by nilesh

--- om.c
+++ om.c
@@ -3,7 +3,10 @@ int main()
 {
     printf("om man shivay 1\n");
     printf("om man shivay 2");
-
+    printf("\n");
     printf("changes happend in prathamesh clone");
+    printf("\n");
+    printf("1");
+    printf("\n");
     printf("changes happend in nilesh clone");
 }
